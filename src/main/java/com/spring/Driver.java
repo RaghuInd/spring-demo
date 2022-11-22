7f2c4078d9442fabed8ce3a59b4e402f74318651
Raghu <rdgowda79@gmail.com>
2022-11-22 11:16:16 +0530

    Spring xml configuration demo

---
+++ src/main/java/com/spring/Driver.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Employee emp = (Employee) context.getBean("emp");
+		System.out.println(emp);
+		Employee emp2 = (Employee) context.getBean("emp2");
+		System.out.println(emp2);
+
+	}
+
+}
